0268565cff00d72b8b9dd97768fbba6f4fef6973
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-02-11 16:51:26 -0300

    Update control.gd

--- flappy-bird/control.gd
+++ flappy-bird/control.gd
@@ -7,12 +7,6 @@ func _ready() -> void:
 	$Panel/Label3.text = "Maior Pontuação: " + str(Global.maior_pontos)
 	
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_button_pressed() -> void:
 	Global.pontos = 0
 	get_tree().change_scene_to_file("res://Cenas/menu_inicial.tscn")
